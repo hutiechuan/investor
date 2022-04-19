ac65cb7f0f7c743514285c132fc33b54fa5adb06
YongfeiTan <tyf6918@gmail.com>
2022-04-19 19:25:50 -0400

    update signup

--- front-end/src/components/signup.js
+++ front-end/src/components/signup.js
@@ -13,7 +13,8 @@ export default class Signup extends Component {
       id: null,
       username: "",
       email: "",
-      password: ""
+      password: "",
+      submitted: false
     };
   }
   onChangeUsername(e) {
@@ -44,7 +45,8 @@ export default class Signup extends Component {
           id: null,
           username: "",
           email: "",
-          password: ""
+          password: "",
+          submitted: true
         });
         //console.log(response.data);
       })
@@ -57,45 +59,60 @@ export default class Signup extends Component {
       id: null,
       username: "",
       email: "",
-      password: ""
+      password: "",
+      submitted: false
     });
   }
   
   render() {
     return (
-      <div className="text-center m-5-auto">
-      <h2>Join us</h2>
-      <h5>Create your personal account</h5>
-      <form>
-          <p>
-              <label>Username</label><br/>
-              <input type="text" 
-                id = "username"
-                name="username"
-                value={this.state.username}
-                onChange={this.onChangeUsername}
-                required />
-          </p>
-          <p>
-              <label>Email address</label><br/>
-              <input type="email" name="email"
-                id = "email"
-                value={this.state.email}
-                onChange={this.onChangeEmail}
-                required />
-          </p>
-          <p>
-              <label>Password</label><br/>
-              <input type="password" name="password"
-                value={this.state.password}
-                onChange={this.onChangePassword}
-               required />
-          </p>
-          <p>
-              <button id="sub_btn" type="button" onClick={this.saveUser}>Register</button>
-          </p>
-      </form>
-      </div>
+      <div className="submit-form">
+        {this.state.submitted ? (
+          <div className="text-center m-5-auto">
+            <h4>You signed up successfully!</h4>
+            <p>
+              <a href="/login" className="button">Log in</a>
+            </p>
+            <p>
+              <button onClick={this.newUser}>Sign up</button>
+            </p>
+          </div>
+        ) : (
+          <div className="text-center m-5-auto">
+          <h2>Join us</h2>
+          <h5>Create your personal account</h5>
+          <form>
+              <p>
+                  <label>Username</label><br/>
+                  <input type="text" 
+                    id = "username"
+                    name="username"
+                    value={this.state.username}
+                    onChange={this.onChangeUsername}
+                    required />
+              </p>
+              <p>
+                  <label>Email address</label><br/>
+                  <input type="email" name="email"
+                    id = "email"
+                    value={this.state.email}
+                    onChange={this.onChangeEmail}
+                    required />
+              </p>
+              <p>
+                  <label>Password</label><br/>
+                  <input type="password" name="password"
+                    value={this.state.password}
+                    onChange={this.onChangePassword}
+                   required />
+              </p>
+              <p>
+                  <button id="sub_btn" type="button" onClick={this.saveUser}>Register</button>
+              </p>
+          </form>
+          </div>
+        )}
+    </div>
     );
   }
   
